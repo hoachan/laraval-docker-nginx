4851489f35a2b25da8922247f010a9292b599518
HOA <hoa@lifestyle-design.jp>
2017-10-04 18:24:34 +0900

    layout struct
    using include in layout to import nav and footer to layout

--- app/blog/resources/views/layout/layout.blade.php
+++ app/blog/resources/views/layout/layout.blade.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
-    @yield('content')
+    @include('layout.nav')
     <div class="container">
-        <div class="content">Hello</div>
+         <div class="content">
+         @yield('content')
+         </div>
     </div>
+    @extends('layout.footer')
 </body>
 </html>
--- app/blog/resources/views/posts/index.blade.php
+++ app/blog/resources/views/posts/index.blade.php
@@ -1,5 +1,7 @@
-@extends('layout/layout')
+@extends('layout.layout')
 
 @section('content')
-
+    <h1>
+        My Application
+    </h1>
 @endsection
